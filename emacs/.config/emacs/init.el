766868636abc0fafeab6674256386e312aab112f
terra2o <terra2o@protonmail.com>
2026-09-08 20:29:30 +0300

    changes made to emacs for readability
    Signed-off-by: terra2o <terra2o@protonmail.com>

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -15,10 +15,17 @@
 (setq inhibit-startup-message t
       make-backup-files nil)
 
+(set-face-attribute 'default nil :font "JetBrains Mono-14:weight=regular")
 (tool-bar-mode -1)
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
 (global-display-line-numbers-mode t)
+
+;; generous line height gives multi-line blocks breathing room
+(setq-default line-spacing 0.2)
+
+;; balance vertical column alignment
+(set-fringe-mode '(12 . 12))
 
 (global-set-key (kbd "C-+") #'text-scale-increase)
 (global-set-key (kbd "C-=") #'text-scale-increase)
@@ -46,7 +53,7 @@
   :mode ("README\\.md\\'" . gfm-mode)
   :init (setq markdown-command "multimarkdown")
   :bind (:map markdown-mode-map
-         ("C-c C-e" . markdown-do)))
+              ("C-c C-e" . markdown-do)))
 
 (use-package gdscript-mode
   :ensure t
@@ -79,6 +86,9 @@
   :config
   (editorconfig-mode 1))
 
+;; enable maximum tree-sitter scope recognition globally
+(setq treesit-font-lock-level 4)
+
 (use-package c-ts-mode
   :mode ("\\.cppm\\'" . c++-ts-mode)
   :init
@@ -88,20 +98,33 @@
   (c-ts-mode-indent-style 'bsd)
   (c-ts-mode-indent-offset 4))
 
+;; strip noisy mode-line lighters that truncate important context
+(use-package diminish
+  :ensure t
+  :config
+  (diminish 'which-key-mode)
+  (diminish 'eldoc-mode)
+  (diminish 'auto-revert-mode)
+  (diminish 'diff-hl-mode)
+  (diminish 'highlight-indent-guides-mode))
+
 (use-package eglot
   :hook ((c-mode c++-mode c-ts-mode c++-ts-mode) . eglot-ensure)
   :init
   (setq eglot-events-buffer-config '(:size nil :format full))
   :config
+  ;; strip blurry inline hints to eliminate irregular horizontal spacing
+  (setq-default eglot-ignored-server-capabilities '(:inlayHintProvider))
+
   (add-to-list 'eglot-server-programs
                '((c-mode c++-mode c-ts-mode c++-ts-mode)
                  . ("clangd")))
-
   (add-to-list 'eglot-server-programs
                '(gdscript-mode . ("localhost" 6005))))
 
 (with-eval-after-load 'eglot
-  (define-key eglot-mode-map (kbd "C-c b") #'eglot-format-buffer))
+  (define-key eglot-mode-map (kbd "C-c b") #'eglot-format-buffer)
+  (add-hook 'eglot-managed-mode-hook (lambda () (eglot-inlay-hints-mode -1))))
 
 (defun my/c-mode-setup ()
   (add-hook 'before-save-hook #'eglot-format-buffer nil t))
@@ -136,15 +159,100 @@
   :init
   (marginalia-mode))
 
-;; (use-package catppuccin-theme
-  ;; :ensure t
-  ;; :config
-  ;; (setq catppuccin-flavor 'mocha)
-  ;; (load-theme 'catppuccin t))
+(use-package consult
+  :ensure t
+  :init
+  (setq consult-async-input-debounce 0.1
+        consult-async-input-throttle 0.2
+        consult-async-refresh-delay 0.15)
+  :config
+  (consult-customize
+   consult-ripgrep consult-git-grep consult-grep
+   consult-line
+   :preview-key '(:debounce 0.2 any)))
 
-(add-to-list 'custom-theme-load-path "~/.config/emacs/themes/")
-(load-theme 'noctalia t)
-(add-hook 'after-init-hook (lambda () (load-theme 'noctalia t)))
+(use-package aperture
+  :vc (:url "https://github.com/mattsawyer77/aperture.el" :rev :newest)
+  :after vertico
+  :config
+  (setq aperture-display 'child-frame
+        aperture-child-frame-width 0.8
+        aperture-child-frame-height 0.6
+        aperture-child-frame-position 'center
+        aperture-child-frame-border-width 1
+        aperture-child-frame-parameters nil))
+
+(use-package catppuccin-theme
+  :ensure t
+  :demand t
+  :init
+  (setq catppuccin-flavor 'mocha)
+  ;; soft charcoal background avoids halation while maintaining crisp luminance
+  (setq catppuccin-mocha-color-overrides
+        '((base     . "#13131a")
+          (mantle   . "#0f0f15")
+          (crust    . "#0a0a0e")
+          (surface0 . "#1e1e28")
+          (surface1 . "#2a2a38")
+          (surface2 . "#3b3b4e")
+          (overlay0 . "#6c7086")
+          (overlay1 . "#7f849c")
+          (overlay2 . "#9399b2")
+          (text     . "#dcdfe7")
+          (subtext0 . "#a6adc8")
+          (subtext1 . "#bac2de")))
+  :config
+  (load-theme 'catppuccin :no-confirm))
+
+(custom-set-faces
+ ;; restful semantic hierarchy: high contrast without optical glare
+ '(default ((t (:background "#13131a" :foreground "#dcdfe7"))))
+ '(font-lock-keyword-face ((t (:foreground "#f38ba8" :weight bold))))
+ '(font-lock-type-face ((t (:foreground "#fab387" :weight bold))))
+ '(font-lock-function-call-face ((t (:foreground "#89b4fa"))))
+ '(font-lock-function-name-face ((t (:foreground "#89b4fa" :weight bold))))
+ '(font-lock-property-name-face ((t (:foreground "#94e2d5"))))
+ '(font-lock-property-use-face ((t (:foreground "#94e2d5"))))
+ '(font-lock-variable-name-face ((t (:foreground "#cdd6f4"))))
+ '(font-lock-constant-face ((t (:foreground "#f9e2af" :weight bold))))
+ '(font-lock-number-face ((t (:foreground "#fab387"))))
+ '(font-lock-string-face ((t (:foreground "#a6e3a1"))))
+ '(font-lock-comment-face ((t (:foreground "#6c7086" :slant italic))))
+ '(font-lock-operator-face ((t (:foreground "#cba6f7"))))
+ '(font-lock-builtin-face ((t (:foreground "#cba6f7" :weight bold))))
+
+ ;; anchored current line and legible margins
+ '(hl-line ((t (:background "#1c1d28"))))
+ '(line-number ((t (:background "#13131a" :foreground "#45475a"))))
+ '(line-number-current-line ((t (:background "#1c1d28" :foreground "#89b4fa" :weight bold))))
+ '(mode-line ((t (:background "#1a1b24" :foreground "#cdd6f4" :box (:line-width 1 :color "#2a2a38")))))
+ '(mode-line-inactive ((t (:background "#101017" :foreground "#585b70" :box (:line-width 1 :color "#181822"))))))
+
+(global-hl-line-mode 1)
+
+;; indent guides highlight active scope without visual noise
+(use-package highlight-indent-guides
+  :ensure t
+  :hook (prog-mode . highlight-indent-guides-mode)
+  :custom
+  (highlight-indent-guides-method 'character)
+  (highlight-indent-guides-responsive 'top)
+  (highlight-indent-guides-delay 0.05)
+  :config
+  (set-face-foreground 'highlight-indent-guides-character-face "#222330")
+  (set-face-foreground 'highlight-indent-guides-top-character-face "#585b70"))
+
+;; soft, desaturated delimiters prevent the "christmas tree" effect
+(use-package rainbow-delimiters
+  :ensure t
+  :hook (prog-mode . rainbow-delimiters-mode)
+  :config
+  (set-face-attribute 'rainbow-delimiters-depth-1-face nil :foreground "#cba6f7")
+  (set-face-attribute 'rainbow-delimiters-depth-2-face nil :foreground "#89b4fa")
+  (set-face-attribute 'rainbow-delimiters-depth-3-face nil :foreground "#94e2d5")
+  (set-face-attribute 'rainbow-delimiters-depth-4-face nil :foreground "#fab387")
+  (set-face-attribute 'rainbow-delimiters-depth-5-face nil :foreground "#f38ba8")
+  (set-face-attribute 'rainbow-delimiters-depth-6-face nil :foreground "#f9e2af"))
 
 (use-package which-key
   :ensure t
@@ -179,6 +287,7 @@
 (use-package evil-mc
   :ensure t
   :after evil
+  :diminish
   :init
   (setq evil-mc-undo-cursors-on-keyboard-quit t)
   :config
@@ -191,16 +300,23 @@
   :after evil
   :ensure t
   :config
+  (setq evil-collection-mode-list
+        (delq 'commentary (delq 'evil-nc evil-collection-mode-list)))
   (evil-collection-init))
 
 (use-package evil-nerd-commenter
   :ensure t
-  :after evil
+  :after (evil general)
+  :diminish
   :config
+  (define-key evil-normal-state-map "gc" nil)
+  (define-key evil-visual-state-map "gc" nil)
+  (define-key evil-motion-state-map "gc" nil)
+
   (general-def
     :states '(normal visual)
-    "gcc" 'evilnc-comment-or-uncomment-lines
-    "gc"  'evilnc-comment-operator))
+    "gcc" #'evilnc-comment-or-uncomment-lines
+    "gc"  #'evilnc-comment-operator))
 
 (use-package iedit
   :ensure t)
@@ -219,7 +335,6 @@
   :ensure t
   :hook ((prog-mode text-mode) . turn-on-diff-hl-mode)
   :config
-  ;; update diff status dynamically in uncommitted buffers without waiting for save
   (diff-hl-flydiff-mode 1))
 
 (use-package treemacs
@@ -258,6 +373,21 @@
   :config
   (elcord-mode 1))
 
+(use-package ligature
+  :ensure t
+  :config
+  (ligature-set-ligatures 'prog-mode
+    '("-=" "==" "!=" "===" "!==" "=>" "->" "<-" "-->" "==>"
+      "//" "///" "/*" "*/" "<!--" "-->" "||" "&&" "<<" ">>"
+      "..." ".." "::" ":::" "++" "--" "+=" "-=" "*=" "/="
+      "==" "!=" "<=" ">=" "<<" ">>" "??" "?:" "||=" "&&="))
+  (global-ligature-mode t))
+
+(use-package hl-todo
+  :ensure t
+  :config
+  (global-hl-todo-mode 1))
+
 (use-package general
   :ensure t
   :demand t
@@ -269,45 +399,41 @@
     :global-prefix "M-SPC")
 
   (my/leader-keys
-    "h"      '(:ignore t :which-key "help/emacs")
-    "hr" '((lambda () (interactive) (load-file user-init-file)) :which-key "reload config")
-    "f"      '(:ignore t :which-key "files")
-    "ff" '(find-file :which-key "find file")
-    "b"      '(:ignore t :which-key "buffers")
-    "bb" '(switch-to-buffer :which-key "switch buffer")
-    "bk" '(kill-current-buffer :which-key "kill buffer")
-    "w"      '(:ignore t :which-key "windows")
-    "wv" '(split-window-right :which-key "split vertical")
-    "ws" '(split-window-below :which-key "split horizontal")
-    "wc" '(delete-window :which-key "close window")
-    "e"      '(:ignore t :which-key "file tree")
-    "ee" '(treemacs :which-key "toggle tree")
-    "ea" '(treemacs-add-project-to-workspace :which-key "add root dir")
-    "er" '(treemacs-remove-project-from-workspace :which-key "remove root dir")
-    "t"      '(:ignore t :which-key "tabs")
-    "tn" '(centaur-tabs-forward :which-key "next tab")
-    "tp" '(centaur-tabs-backward :which-key "previous tab")
-    "g"      '(:ignore t :which-key "git")
-    "gg" '(magit-status :which-key "status")
-    "gb" '(magit-blame :which-key "blame")
-    "gl" '(magit-log-buffer-file :which-key "file log")
-    "gd" '(magit-diff-dwim :which-key "diff")))
+    "SPC" '(consult-ripgrep :which-key "live grep")
+    "/"   '(consult-line :which-key "search buffer")
+    "h"   '(:ignore t :which-key "help/emacs")
+    "hr"  '((lambda () (interactive) (load-file user-init-file)) :which-key "reload config")
+    "f"   '(:ignore t :which-key "files")
+    "ff"  '(find-file :which-key "find file")
+    "b"   '(:ignore t :which-key "buffers")
+    "bb"  '(consult-buffer :which-key "switch buffer")
+    "bk"  '(kill-current-buffer :which-key "kill buffer")
+    "s"   '(:ignore t :which-key "search")
+    "sp"  '(consult-ripgrep :which-key "live grep project")
+    "sd"  '(consult-find :which-key "find file by name")
+    "sl"  '(consult-line :which-key "search lines")
+    "w"   '(:ignore t :which-key "windows")
+    "wv"  '(split-window-right :which-key "split vertical")
+    "ws"  '(split-window-below :which-key "split horizontal")
+    "wc"  '(delete-window :which-key "close window")
+    "e"   '(:ignore t :which-key "file tree")
+    "ee"  '(treemacs :which-key "toggle tree")
+    "ea"  '(treemacs-add-project-to-workspace :which-key "add root dir")
+    "er"  '(treemacs-remove-project-from-workspace :which-key "remove root dir")
+    "t"   '(:ignore t :which-key "tabs")
+    "tn"  '(centaur-tabs-forward :which-key "next tab")
+    "tp"  '(centaur-tabs-backward :which-key "previous tab")
+    "g"   '(:ignore t :which-key "git")
+    "gg"  '(magit-status :which-key "status")
+    "gb"  '(magit-blame :which-key "blame")
+    "gl"  '(magit-log-buffer-file :which-key "file log")
+    "gd"  '(magit-diff-dwim :which-key "diff")))
 
 (custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(cape catppuccin-theme centaur-tabs clang-format corfu diff-hl elcord
-	  evil-collection evil-mc evil-multiedit evil-nerd-commenter
-	  gdscript-mode general highlight-indent-guides magit
-	  marginalia markdown-mode orderless treemacs-evil vertico
-	  yafolding)))
-
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+   '(cape catppuccin-theme centaur-tabs clang-format consult corfu
+      diff-hl diminish elcord evil-collection evil-mc
+      evil-multiedit evil-nerd-commenter gdscript-mode general
+      highlight-indent-guides hl-todo ligature magit marginalia
+      markdown-mode orderless rainbow-delimiters treemacs-evil
+      vertico yafolding)))
